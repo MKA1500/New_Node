2f0f84b5a61782ef97119b8165a173c9817006f5
MKA1500 <maciejkabsch@gmail.com>
2017-07-02 14:01:13 +0200

    work in progress

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    // GET/READ
+    $('#get-button').on('click', function(){
+        $.ajax({
+            url: '/products',
+            contentType: 'application/json',
+            success: function(response){
+                console.log(response);
+            }
+        })
+    });
+});
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
 
+var products = [
+    {
+        id: 1,
+        name: 'tablet'
+    },
+    {
+        id: 2,
+        name: 'smartphone'
+    }
+];
+
+var currentId = 2;
+
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname));
+
+app.get('/products', function(req, res){
+    res.send('Success!');
+})
 
 app.listen(PORT, function(){
     console.log('Server listening on ' + PORT);
