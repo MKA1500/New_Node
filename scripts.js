1ea706774238fe286da335b6e155c14fe9555a3b
MKA1500 <maciejkabsch@gmail.com>
2017-07-02 14:56:01 +0200

    work in progress

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
     // GET/READ
-    $('#get-button').on('click', function(){
+    $('#get-button').on('click', function () {
         $.ajax({
             url: '/products',
             contentType: 'application/json',
-            success: function(response){
-                console.log(response);
+            success: function (response) {
+                var tbodyEl = $('tbody');
+                tbodyEl.html('');
+                response.products.forEach(function (product) {
+                    tbodyEl.append('<tr><td class="id">' + product.id + '</td><td><input type="text" class="name form-control" value="' + product.name + '"></input></td><td><button class="update-button btn btn-info">UPDATE / PUT</button><button class="update-delete btn btn-danger">DELETE</button></td><tr></tr>');
+                });
             }
         })
     });
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 app.get('/products', function(req, res){
-    res.send('Success!');
+    res.send({products: products});
 })
 
 app.listen(PORT, function(){
