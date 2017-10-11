203f951e22ff684efce5ecdd5796a0c19ed1a12c
MKA1500 <maciejkabsch@gmail.com>
2017-10-11 18:29:01 +0200

    gulp watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
+// concat -> gulp t1
+
 gulp.task('t1', function(){
 return gulp.src('src/**/*.js')
     .pipe(concat('all.js'))
     .pipe(gulp.dest('dest'))
+});
+
+// watcher -> gulp w1
+
+gulp.task('w1', function(){
+    gulp.watch('src/**/*.js', ['t1']);
 });
